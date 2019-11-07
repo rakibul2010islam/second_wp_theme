2cd9a135ae8e09fb986667b2df6a8d81893ac0e2
unknown <rakibul2010islam@gmail.com>
2019-11-07 21:15:46 +0600

    front-page-recovered

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 
 <div>
 	<div class="main-column">
-		<?php if(have_posts()): ?>
-			<?php while(have_posts()): the_post();?>
-				<?php get_template_part('content', get_post_format()); ?>
-			<?php endwhile; ?>
+		<div class="left-column">
+			<?php if(have_posts()): ?>
+				<?php while(have_posts()): the_post();?>
+					<?php get_template_part('content', get_post_format()); ?>
+				<?php endwhile; ?>
 
-			<?php echo paginate_links(); ?>
-		<?php else: ?>
-			<?php echo wpautop("No posts found"); ?>
-		<?php endif; ?>
+				<?php echo paginate_links(); ?>
+			<?php else: ?>
+				<?php echo wpautop("No posts found"); ?>
+			<?php endif; ?>
+		</div>
+
+		<?php get_sidebar(); ?>
 	</div>
 
 </div>
